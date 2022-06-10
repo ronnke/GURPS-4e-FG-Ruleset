8c00de4e7c32d994038139bba0236809cb2a9e99
Brian Ronnle <ronnke@gmail.com>
2022-06-11 01:17:37 +1000

    Refactored language code

---
+++ scripts/manager_language2.lua
@@ -0,0 +1,8 @@
+--
+-- Please see the license.html file included with this distribution for
+-- attribution and copyright information.
+--
+
+function onInit()
+	LanguageManager.CHAR_LANGUAGE_LIST = "traits.languagelist";
+end
