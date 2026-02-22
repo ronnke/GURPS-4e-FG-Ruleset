d74349023512b0ebe67db9ffc0fee043ad69021a
ronnke <ronnke@gmail.com>
2026-02-22 23:37:53 +1000

    Fixed Combat Tracker status color

--- ct/scripts/clientct_entry.lua
+++ ct/scripts/clientct_entry.lua
@@ -7,42 +7,53 @@ function onInit()
 	if super and super.onInit then
 		super.onInit();
 	end
-	self.onHealthChanged();
+
+    local node = getDatabaseNode();
+    DB.addHandler(DB.getPath(node, "injury"), "onUpdate", self.onInjuryChanged);
+    DB.addHandler(DB.getPath(node, "fatigue"), "onUpdate", self.onFatigueChanged);
+
+	self.onInjuryChanged();
 	self.onFatigueChanged();
+end
+
+function onClose()
+    local node = getDatabaseNode();
+	DB.removeHandler(DB.getPath(node, "injury"), "onUpdate", self.onInjuryChanged);
+	DB.removeHandler(DB.getPath(node, "fatigue"), "onUpdate", self.onFatigueChanged);
 end
 
 function onFactionChanged()
 	super.onFactionChanged();
 	updateHealthDisplay();
 end
-function onHealthChanged()
-	local _, _, sColor = ActorManagerGURPS4e.getInjuryStatus(getDatabaseNode());
-
+function onInjuryChanged()
+	local rActor = ActorManager.resolveActor(getDatabaseNode())
+	local _, _, sColor = ActorManagerGURPS4e.getInjuryStatus(rActor);
 	hps.setColor(sColor);
 	status.setColor(sColor);
 end
 function onFatigueChanged()
-	local _, _, sColor = ActorManagerGURPS4e.getFatigueStatus(getDatabaseNode());
-
+	local rActor = ActorManager.resolveActor(getDatabaseNode())
+	local _, _, sColor = ActorManagerGURPS4e.getFatigueStatus(rActor);
 	fps.setColor(sColor);
 end
 
 function updateHealthDisplay()
-	local sFaction = friendfoe.getStringValue();
-    local sOptSHPC = OptionsManager.getOption("SHPC");
-    local sOptSHNPC = OptionsManager.getOption("SHNPC");
-    local bShowDetail = (sFaction == "friend" and sOptSHPC == "detailed") or (sFaction ~= "friend" and sOptSHNPC == "detailed");
-    local bStatusOff = (sFaction == "friend" and sOptSHPC == "off") or (sFaction ~= "friend" and sOptSHNPC == "off") or bShowDetail;
+	local sFaction = friendfoe.getValue();
+	local sOption = (sFaction == "friend") and OptionsManager.getOption("SHPC") or OptionsManager.getOption("SHNPC");
+
+	local bShowDetail = (sOption == "detailed");
+	local bShowStatus = (sOption == "status");
 
 	hps.setVisible(bShowDetail);
 	fps.setVisible(bShowDetail);
 	injury.setVisible(bShowDetail);
 	fatigue.setVisible(bShowDetail);
-	status.setVisible(not bStatusOff);
+	status.setVisible(bShowStatus);
 end
 
 function updateShowOrder()
-	local sFaction = friendfoe.getStringValue();
+	local sFaction = friendfoe.getValue();
 	local sOptCTSI = OptionsManager.getOption("CTSI");
 	local bShowSpeed = ((sOptCTSI == "friend") and (sFaction == "friend")) or (sOptCTSI == "on");
     speed.setVisible(bShowSpeed);
